755ca86cca961c9a6d38e7397cdea0f09168dae0
leandrohcn <leandro1111bfr@gmail.com>
2025-07-04 13:41:53 -0300

    ola

---
+++ src/main/java/baggagio/com/individual_challenge/IndividualChallengeApplication.java
@@ -0,0 +1,13 @@
+package baggagio.com.individual_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndividualChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndividualChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/baggagio/com/individual_challenge/IndividualChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package baggagio.com.individual_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndividualChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
