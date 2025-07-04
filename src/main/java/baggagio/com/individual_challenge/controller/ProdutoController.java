fa8ed34a7d92f790344a7e8bdd34ea966d9a1905
leandrohcn <leandro1111bfr@gmail.com>
2025-07-04 15:09:47 -0300

    feature: Classe que vai lidar com as requisições

---
+++ src/main/java/baggagio/com/individual_challenge/controller/ProdutoController.java
@@ -0,0 +1,52 @@
+package baggagio.com.individual_challenge.controller;
+
+import baggagio.com.individual_challenge.dtos.ProdutoDTO;
+import baggagio.com.individual_challenge.dtos.ProdutoResponseDTO;
+import baggagio.com.individual_challenge.entities.Produto;
+import baggagio.com.individual_challenge.services.ProdutoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+    private final ProdutoService produtoService;
+
+    public ProdutoController(ProdutoService produtoService) {
+        this.produtoService = produtoService;
+    }
+    // Requisicoes
+    @GetMapping
+    public ResponseEntity<List<Produto>> buscaTodosProdutos() {
+        List<Produto> produtos = produtoService.listarTodosProdutos();
+        return ResponseEntity.ok(produtos);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Produto> buscaProdutoPorId(@PathVariable Integer id) {
+        Produto produto = produtoService.buscarProdutoPorId(id);
+        return ResponseEntity.ok(produto);
+    }
+
+    @PostMapping
+    public ResponseEntity<ProdutoResponseDTO> criaProduto(@RequestBody ProdutoDTO novoProduto) {
+        ProdutoResponseDTO produto = produtoService.criarProduto(novoProduto);
+        return new ResponseEntity<>(produto, HttpStatus.CREATED);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<ProdutoResponseDTO> atualizaProduto(@PathVariable Integer id, @RequestBody ProdutoDTO novoProduto) {
+        ProdutoResponseDTO produto = produtoService.atualizarProduto(novoProduto, id);
+        return ResponseEntity.ok(produto);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deletaProduto(@PathVariable Integer id) {
+        produtoService.excluirProduto(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
