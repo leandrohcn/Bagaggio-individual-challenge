4582035cef0ee8f75fe6eb428e19d7d1d2aeeb22
leandrohcn <leandro1111bfr@gmail.com>
2025-07-05 16:26:45 -0300

    feature: implementação para busca por outros parametros, que nao Id

--- src/main/java/com/baggaggio/individual_challenge/repositories/ProdutoRepository.java
+++ src/main/java/com/baggaggio/individual_challenge/repositories/ProdutoRepository.java
@@ -1,8 +1,14 @@
 package com.baggaggio.individual_challenge.repositories;
 
+import com.baggaggio.individual_challenge.entities.Categoria;
 import com.baggaggio.individual_challenge.entities.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
 // Repository que "extend" o JpaRepositry = facilita a persistência de dados (simplifica o acesso ao banco)
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
-
+    Optional<Produto> findByNome(String nome);
+    List<Produto> findByCategoria(Categoria categoria);
 }
