5ebe3c0d9e60c00791b1dd73fcc18f76c2dadae4
leandrohcn <leandro1111bfr@gmail.com>
2025-07-05 16:27:51 -0300

    fix: mudanças quanto ao retorno e explicações sobre o que foi feito

--- src/main/java/com/baggaggio/individual_challenge/controller/ProdutoController.java
+++ src/main/java/com/baggaggio/individual_challenge/controller/ProdutoController.java
@@ -1,14 +1,18 @@
 package com.baggaggio.individual_challenge.controller;
 
+import com.baggaggio.individual_challenge.dtos.AtualizaProdutoDTO;
 import com.baggaggio.individual_challenge.dtos.ProdutoDTO;
 import com.baggaggio.individual_challenge.dtos.ProdutoResponseDTO;
+import com.baggaggio.individual_challenge.entities.Categoria;
 import com.baggaggio.individual_challenge.entities.Produto;
 import com.baggaggio.individual_challenge.services.ProdutoService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/produtos")
@@ -18,28 +22,48 @@ public class ProdutoController {
     public ProdutoController(ProdutoService produtoService) {
         this.produtoService = produtoService;
     }
+
+
+    // delimitei ao service tratar das exceções de acordo com regras de negocio
+    // E as excecoes estao sendo tratadas com @ExceptionHandler no pacote de exceptions, para uma melhor visualização ao deparar com erros em requisições
+
     // Requisicoes
+    // As que precisam retornar algo, implementei o retorno para os DTOS, pois numa aplicação a camada de acesso a dados (geralmente um banco de dados)
+    // não precisa interagir com a camada de serviços, reduzindo o acoplamento.
+
     @GetMapping
-    public ResponseEntity<List<Produto>> buscaTodosProdutos() {
-        List<Produto> produtos = produtoService.listarTodosProdutos();
-        return ResponseEntity.ok(produtos);
+    public ResponseEntity<List<ProdutoResponseDTO>> buscaTodosProdutos(@RequestParam (required = false) Categoria categoria) {
+        List<Produto> produtos;
+        if (categoria == null) {
+            // se categoria nao passada na requisição, entao retorna todas ou nenhuma
+            produtos = produtoService.listarTodosProdutos();
+        }
+        else {
+            // retorna todos os produtos que tem a categoria passada na requisição
+            produtos = produtoService.listarPorCategoria(categoria);
+        }
+
+        List<ProdutoResponseDTO> resposta =  produtos.stream().map(ProdutoResponseDTO::new).toList();
+        return new ResponseEntity<>(resposta, HttpStatus.OK);
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<Produto> buscaProdutoPorId(@PathVariable Integer id) {
+    public ResponseEntity<ProdutoResponseDTO> buscaProdutoPorId(@PathVariable Integer id) {
         Produto produto = produtoService.buscarProdutoPorId(id);
-        return ResponseEntity.ok(produto);
+        ProdutoResponseDTO resposta = new ProdutoResponseDTO(produto);
+        return ResponseEntity.ok(resposta);
     }
 
     @PostMapping
-    public ResponseEntity<ProdutoResponseDTO> criaProduto(@RequestBody ProdutoDTO novoProduto) {
+    public ResponseEntity<ProdutoResponseDTO> criaProduto(@Valid @RequestBody ProdutoDTO novoProduto) {
         ProdutoResponseDTO produto = produtoService.criarProduto(novoProduto);
         return new ResponseEntity<>(produto, HttpStatus.CREATED);
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<ProdutoResponseDTO> atualizaProduto(@PathVariable Integer id, @RequestBody ProdutoDTO novoProduto) {
-        ProdutoResponseDTO produto = produtoService.atualizarProduto(novoProduto, id);
+    public ResponseEntity<ProdutoResponseDTO> atualizaProduto(@PathVariable Integer id,
+                                                              @Valid @RequestBody AtualizaProdutoDTO produtoAtualizado) {
+        ProdutoResponseDTO produto = produtoService.atualizarProduto(produtoAtualizado, id);
         return ResponseEntity.ok(produto);
     }
 
