34a71ba42b93775325313bc91f616843586cff63
leandrohcn <leandro1111bfr@gmail.com>
2025-07-05 16:26:07 -0300

    feature: DTOS

--- src/main/java/com/baggaggio/individual_challenge/dtos/ProdutoDTO.java
+++ src/main/java/com/baggaggio/individual_challenge/dtos/ProdutoDTO.java
@@ -1,12 +1,32 @@
 package com.baggaggio.individual_challenge.dtos;
 
+import com.baggaggio.individual_challenge.entities.Categoria;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 //DTO para criação e atualização de produtos
 @Data
+@Schema(description = "Representa dados que vão ser passados no corpo da requisição")
 public class ProdutoDTO {
+    @JsonProperty(required = true)
+    @NotBlank
     private String nome;
+    @JsonProperty(required = true)
+    @NotBlank
     private String descricao;
+    @JsonProperty(required = true)
+    @NotNull(message = "O preço não pode ser null.")
+    @Positive(message = "O preço deve ser um valor positivo.")
     private Float preco;
+    @JsonProperty(required = true)
+    @NotNull(message = "A quantidade não pode ser null.")
+    @Positive(message = "Quantidade deve ser um valor positivo.")
     private Integer quantidade;
+    @JsonProperty(required = true)
+    @NotNull(message = "Categoria não pode ser null.")
+    private Categoria categoria;
 }
--- src/main/java/com/baggaggio/individual_challenge/dtos/ProdutoResponseDTO.java
+++ src/main/java/com/baggaggio/individual_challenge/dtos/ProdutoResponseDTO.java
@@ -1,16 +1,20 @@
 package com.baggaggio.individual_challenge.dtos;
 
+import com.baggaggio.individual_challenge.entities.Categoria;
 import com.baggaggio.individual_challenge.entities.Produto;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 //DTO destinado para resposta da requisição
 @Data
+@Schema(description = "Representa a resposta que vai ser passada na requisição")
 public class ProdutoResponseDTO {
     private Integer id;
     private String nome;
     private String descricao;
     private float preco;
     private int quantidade;
+    private Categoria categoria;
 //Construtor para que ao inves de passar a entidade na resposta, eu passe o DTO.
 //Por mais que não tenha dados sensíveis nem nada do tipo.
     public ProdutoResponseDTO(Produto produto) {
@@ -19,5 +23,6 @@ public class ProdutoResponseDTO {
         this.descricao = produto.getDescricao();
         this.preco = produto.getPreco();
         this.quantidade = produto.getQuantidade();
+        this.categoria = produto.getCategoria();
     }
 }
